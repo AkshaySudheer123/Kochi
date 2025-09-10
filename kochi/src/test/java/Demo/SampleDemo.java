63b53d5eed6745c1fdecbb0657d9196566ac6834
Akshay Sudheer <Akshay Sudheer@DESKTOP-V4ALAVD>
2025-09-10 12:01:40 +0530

    commit

---
+++ kochi/src/test/java/Demo/SampleDemo.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		System.out.println("kochi students");
+		System.out.println("kochi students");
+		System.out.println("kochi students");
+		System.out.println("kochi students");
+		System.out.println("kochi students");
+
+	}
+
+}
